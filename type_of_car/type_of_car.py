2793eb643042456a488744e16a95272905723719
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-06-05 00:53:25 +0800

    making car blueprint, added accelerate and brakes

--- type_of_car/type_of_car.py
+++ type_of_car/type_of_car.py
@@ -1,2 +1,14 @@
 class Car:
+    def __init__(self, year_model, make): #private attributes
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+
+    def accelerate(self):
+        self.__speed += 5 #adds 5 speed when called
+
+    def brake(self):
+        self.__speed -= 5 #decrease speed when called
+        if self.__speed < 0: self.__speed = 0
+
     
