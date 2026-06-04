02dd2f15d4368831b2db086b30702e06fb3be841
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-06-05 01:08:14 +0800

    created constructor and start test run

--- type_of_car/car_simulator.py
+++ type_of_car/car_simulator.py
@@ -1,3 +1,10 @@
 from type_of_car import Car
 
 class CarSimulator:
+    def __init__(self, year_model, make):
+        self.my_car = Car(year_model, make)
+
+    def test_run(self):
+        print(" Starting test run for {self.my_car.get_year_model()} {self.my_car.get_make()}")
+        print("\nAccelerating....")
+        
