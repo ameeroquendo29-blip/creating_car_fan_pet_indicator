6bda013d3b108f49cbd818c89c505cf48d13fdaf
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-06-05 02:34:19 +0800

    forgot to make deceleration

--- type_of_car/car_simulator.py
+++ type_of_car/car_simulator.py
@@ -11,8 +11,16 @@ class CarSimulator:
             self.my_car.accelerate()
             print(f"  Stomp {stomp}: Speed is now {self.my_car.get_speed()} km/h")
 
+        print("\n[Action] Applying Brakes...")
+        for step in range(1, 6):
+            self.my_car.brake()
+            print(f"  Step {step}: Speed is now {self.my_car.get_speed()} km/h")
+
         print("\nTest run complete....")
 
 def simulation():
     simulation = CarSimulator("2024", "Toyota")
     simulation.test_run()
+
+if __name__ == "__main__":
+    simulation()
