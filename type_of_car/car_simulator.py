bd2ba77aa67dff2158533587e6b1f0b299759c7c
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-06-05 02:44:32 +0800

    put all print f function to be more stable

--- type_of_car/car_simulator.py
+++ type_of_car/car_simulator.py
@@ -5,18 +5,18 @@ class CarSimulator:
         self.my_car = Car(year_model, make)
 
     def test_run(self):
-        print(" Starting test run for {self.my_car.get_year_model()} {self.my_car.get_make()}")
-        print("\nAccelerating....")
+        print(f" Starting test run for {self.my_car.get_year_model()} {self.my_car.get_make()}")
+        print(f"\nAccelerating....")
         for stomp in range(1, 6):
             self.my_car.accelerate()
             print(f"  Stomp {stomp}: Speed is now {self.my_car.get_speed()} km/h")
 
-        print("\nApplying Brakes...")
+        print(f"\nApplying Brakes...")
         for step in range(1, 6):
             self.my_car.brake()
             print(f"  Step {step}: Speed is now {self.my_car.get_speed()} km/h")
 
-        print("\nTest run complete....")
+        print(f"\nTest run complete....")
 
 def simulation():
     simulation = CarSimulator("2024", "Toyota")
