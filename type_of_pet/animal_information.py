acdf6ae8a8ca81a0129cddd07050d3a1878ab816
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-06-05 03:00:32 +0800

    for collecting of data

--- type_of_pet/animal_information.py
+++ type_of_pet/animal_information.py
@@ -3,3 +3,10 @@ from type_of_pet import Pet
 class AnimalInformation:
     def __init__(self):
         self.pet_info = Pet()
+
+    def collect_pet_data(self):
+        print("----- Input Pet Details -----")
+        name = input("Enter pet name: ")
+        animal_type = input("Enter animal type (ex., Dog, Cat, Bird): ")
+
+        
