c215d6c9991dbd47236d7c40285cb6b78c0f29c6
ameeroquendo29-blip <ameeroquendo29@gmail.com>
2026-06-04 23:34:34 +0800

    created test fan 1

--- speed_of_fan/test_fan.py
+++ speed_of_fan/test_fan.py
@@ -2,4 +2,10 @@ from speed_of_fan import Fan
 
 class TestFan:
     def test_fan():
+        fan1 = Fan()
+        fan1.set_speed(Fan.FAST)  # Maximum speed
+        fan1.set_radius(10.0)
+        fan1.set_color("yellow")
+        fan1.set_on(True)
+
         
